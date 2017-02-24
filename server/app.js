be14f51c227e48ec64d4066cccafce517f1b6e9c
BakharEugene <eugenebakhar@gmail.com>
2017-02-24 16:44:13 +0300

    some fixes

---
+++ server/app.js
@@ -0,0 +1,3 @@
+var User=require("./models/user.js")
+var eugene=new User("Eugene","Bakhar",20);
+eugene.sayHi();
--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,12 @@ this.name=name;
 this.familyname=familyname;
 this.age=age;
 this.displayinfo=function(){
-    console.log("Name: ${this.name} Family name: ${this.familyname} Age:${this.age} ")
+    console.log(`Name: ${this.name} Family name: ${this.familyname} Age:${this.age}` )
 }
 }
+User.prototype.sayHi = function() {
+	this.displayinfo();
+    console.log(`Привет, меня зовут ${this.name}`);
+};
+module.exports=User;
+
