4519685d187deb68820d66a3bda092d88eb6f7ad
BakharEugene <eugenebakhar@gmail.com>
2017-02-24 15:54:26 +0300

    server.js and index.js added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 8082;
-
+function start(){
 const server = http.createServer((request, resolve) => {
   resolve.statusCode = 200;
   resolve.setHeader('Content-Type', 'text/plain');
@@ -11,4 +11,6 @@ const server = http.createServer((request, resolve) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
+}
+exports.start=start;
